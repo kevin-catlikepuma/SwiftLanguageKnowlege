d0fcb551ffa4114705881563d242c7b0ab2d2973
Kevin Wei <wei_wen1016@163.com>
2016-03-23 08:23:10 +0800

    update to collectionview

---
+++ Controls/CollectionView/FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by WeiWen on 16/3/23.
+//  Copyright © 2016年 WeiWen. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
